091576cb2f33c1e610f310435804fe31e82ee1d2
Adrian Tofan <adrian@tofan.co>
2014-12-24 14:14:54 +0100

    Initial Commit

---
+++ SelfSizing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelfSizing
+//
+//  Created by Adrian Tofan on 24/12/14.
+//  Copyright (c) 2014 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
